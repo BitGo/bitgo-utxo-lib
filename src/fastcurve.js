8000849252ec4cea6482f092bce25391a778ac91
Tyler Levine <tyler@bitgo.com>
2018-11-19 11:28:37 -0800

    Add doc comments for fastcurve functions
    Signed-off-by: Tyler Levine <tyler@bitgo.com>

--- src/fastcurve.js
+++ src/fastcurve.js
@@ -35,6 +35,16 @@ var publicKeyCreate = function (buffer, compressed) {
   return secp256k1.publicKeyCreate(buffer, compressed)
 }
 
+/**
+ * Create an ECDSA signature over the given message hash `hash` with
+ * the private key `d`.
+ *
+ * Uses secp256k1 for acceleration. If secp256k1 is not available,
+ * this function returns undefined.
+ * @param hash {Buffer} hash of the message which is to be signed
+ * @param d {BigInteger} private key which is to be used for signing
+ * @return {ECSignature}
+ */
 var sign = function (hash, d) {
   typeforce(types.tuple(types.Buffer256bit, types.BigInt), arguments)
 
@@ -46,6 +56,17 @@ var sign = function (hash, d) {
   return ECSignature.fromDER(secp256k1.signatureExport(sig))
 }
 
+/**
+ * Verify an ECDSA signature over the given message hash `hash` with signature `sig`
+ * and public key `pubkey`.
+ *
+ * Uses secp256k1 for acceleration. If secp256k1 is not available,
+ * this function returns undefined.
+ * @param hash {Buffer} hash of the message which is to be verified
+ * @param sig {ECSignature} signature which is to be verified
+ * @param pubkey {Buffer} public key which will be used to verify the message signature
+ * @return {Boolean}
+ */
 var verify = function (hash, sig, pubkey) {
   typeforce(types.tuple(
     types.Hash256bit,
